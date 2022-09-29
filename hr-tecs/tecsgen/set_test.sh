988fb78cb8c883b1fd81af64f24943d38636bc01
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2022-09-29 18:36:42 +0900

    set_test.shの10～17行目

--- hr-tecs/tecsgen/set_test.sh
+++ hr-tecs/tecsgen/set_test.sh
@@ -1,5 +1,8 @@
 BASEDIR=$PWD    # This line is to be changed if you put the contents into your rc file.
 
-echo $PATH
-echo $BASEDIR
-grep "$BASEDIR/tecsgen" 
+echo $PATH | grep "$BASEDIR/tecsgen" > /dev/null
+if [ $? = 0 ]; then
+	echo "PATH: unchanged !!"
+else
+	PATH=$BASEDIR/tecsgen:$PATH
+fi
