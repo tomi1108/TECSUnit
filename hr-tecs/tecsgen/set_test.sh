96391b460ed597ddf7e634bcb8dd6fb002f5131d
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2022-10-03 13:17:04 +0900

    /dev/nullの確認

--- hr-tecs/tecsgen/set_test.sh
+++ hr-tecs/tecsgen/set_test.sh
@@ -1,17 +1,3 @@
 BASEDIR=$PWD    # This line is to be changed if you put the contents into your rc file.
 
-#echo $PATH | grep "$BASEDIR/tecsgen" > /dev/null
-echo "test koko"
-if [ $? = 0 ]; then
-	echo "PATH: unchanged !!"
-else
-	echo "test if"
-	PATH=$BASEDIR/tecsgen:$PATH
-fi
-echo "test end"
-if [ "$TECSPATH" = ""  ]; then
-	export TECSPATH=$BASEDIR/tecsgen/tecs    # actual path
-#	export TECSPATH=$BASEDIR/tecs            # symbolic link
-else
-	echo "TECSPATH: unchanged !! ($TECSPATH)"
-fi
+echo "Hello World" > /dev/null
