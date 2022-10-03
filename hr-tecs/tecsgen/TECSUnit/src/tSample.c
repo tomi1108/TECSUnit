f8b7c5e5ef0bca341349c94f4053fdcb877b7872
tomi1108 <h.tomimori.290@ms.saitama-u.ac.jp>
2022-10-03 14:13:37 +0900

    .gitignore,src/～の追加

---
+++ hr-tecs/tecsgen/TECSUnit/src/tSample.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+#include "tSample_tecsgen.h"
+
+#ifndef E_OK
+#define E_OK 0
+#define E_ID (-18)
+#endif
+
+ER
+eEnt_sayHello(CELLIDX idx, int32_t times)
+{
+	ER ercd = E_OK;
+	CELLCB *p_cellcb;
+	if (VALID_IDX(idx)) {
+		p_cellcb = GET_CELLCB(idx);
+	}
+	else {
+		return(E_ID);
+	}
+
+
+	printf( "Sample: " );
+	while( times-- > 0 )
+		printf( "hello " );
+	printf( "\n" );
+
+	return(ercd);
+}
+
+ER
+eEnt_howAreYou(CELLIDX idx, char_t* buf, int32_t len)
+{
+	ER ercd = E_OK;
+	CELLCB *p_cellcb;
+	if (VALID_IDX(idx)) {
+		p_cellcb = GET_CELLCB(idx);
+	}
+	else {
+		return(E_ID);
+	}
+
+	strncpy( buf, "Sample: I'm fine!", len );
+
+	return(ercd);
+} 
---
+++ hr-tecs/tecsgen/TECSUnit/src/tSimple.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "tSimple_tecsgen.h"
+
+#ifndef E_OK
+#define	E_OK	0		/* 正常終了 */
+#define	E_ID	(-18)	/* 不正ID番号 */
+#endif
+
+void
+eBody_main(CELLIDX idx)
+{
+	CELLCB *p_cellcb;
+	char buf[256];
+	if (VALID_IDX(idx)) {
+		p_cellcb = GET_CELLCB(idx);
+	}
+	else {
+
+	}
+
+#define N_HELLO 3
+	printf( "Simple: Say hello %d times.\n", N_HELLO );
+	cCall_sayHello( N_HELLO );
+
+	printf( "Simple: How are you?\n" );
+	cCall_howAreYou( buf, sizeof buf );
+
+	puts( buf );
+}
